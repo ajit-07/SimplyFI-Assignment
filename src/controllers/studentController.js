42667ee94dd2bfe2b2ece49b8beff9d5adb77a51
ajit-07 <sajit1619@gmail.com>
2023-01-11 10:14:42 +0530

    final check done

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,13 +1,28 @@
-const csv = require('csv-parser');
-const fs = require('fs')
+const csv = require('csv-parser');                       //Library for streaming CSV data and parsing it into JS objects
+const fs = require('fs')                                 //Build in module of Node-JS used for interacting with file system
 const studentModel = require('../models/studentModel.js')
+
+//Handler function for uploading the file and writing its content to DB
 
 const fileUpload = async (req, res) => {
     try {
+        //Checking the file is present in request object or not and accepts only csv file.
         if (!req.file) return res.status(400).send("No file was provided.");
         if (req.file.mimetype != 'text/csv') return res.status(400).send('You can only upload csv file')
 
         let items = []
+        /*fs.createReadStream==>It is a method from built-in fs module used to create a readable stream for a specified file
+        It takes path of the file as its argument and returns readable stream object.
+
+        .pipe(csv({}))-It is a chaining method on the readable stream returned by fs.createreadstream.It basically
+        connects the readable stream to writable stream.
+        csv()=>It is function from csv-parser that returns a transform stream that is used to parse data from readablestream.
+        It parses the transform stream to JS objects where every object is row of csv file and the properties of object
+        are contents of the row.
+
+        ON method=> is the event listener which is emitted from the stream to handle the data and after that perform some 
+        operations on it.*/
+
         fs.createReadStream(req.file.path)
             .pipe(csv({}))
             .on('data', (data) => items.push(data))
@@ -68,6 +83,7 @@ const fileUpload = async (req, res) => {
     }
 }
 
+//This is a handler unction to get the result of the students by their id in path params.
 
 const getResult = async (req, res) => {
     try {
@@ -86,11 +102,18 @@ const getResult = async (req, res) => {
     }
 }
 
+//This is the handler function to get the list of students passed/failed by query params.
+
 const getResultStatus = async (req, res) => {
     try {
         let { resultStatus } = req.query
-        if (Object.keys(req.query).length == 0) return res.status(400).send({ status: false, message: `Please enter query details to fetch result` })
 
+        //Sends an erroe if user sends nothing in query params
+
+        if (Object.keys(req.query).length == 0) return res.status(400).send({ status: false, message: `Please enter query details to fetch result` })
+        
+        //Checks that resultStatus can contain only two values passed or failed.
+        
         let arr = ['Passed', 'Failed']
         if (arr.indexOf(resultStatus) == -1) return res.status(400).send({ status: false, message: `Result status can accept only 'Passed' or 'Failed'` })
 
--- src/models/studentModel.js
+++ src/models/studentModel.js
@@ -30,6 +30,10 @@ const studentSchema = new mongoose.Schema({
     resultStatus: String
 }, { timestamps: true })
 
+/*It is a mongoose pre-save middleware function which basically helps to calculate total marks and save result status 
+according to the criteria mentioned before saving the document.
+After calculating it calls the next middleware function to call the next function in save process.*/
+
 studentSchema.pre('save', function (next) {
     this.totalMarks = this.Mark1 + this.Mark2 + this.Mark3;
     let avg = this.totalMarks / 3;
